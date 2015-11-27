99192db41ae9da0c1d34101d72876d446790cb27
JY <jiaying.hu@hotmail.com>
2015-11-27 17:00:05 +0100

    merge fix

--- Unity/Menu/Main Menu/Assets/Scripts/RotateScript.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class RotateScript: MonoBehaviour {
-
-	void Update () {
-		transform.Rotate (new Vector3 (0, 30, 0) * Time.deltaTime);
-	}
-}
