b2be58f2b574e9e6aca1922ab918e5c69cd2fa58
Thomas Broens <Thomas Broens>
2015-11-27 17:29:10 +0100

    Nerd opnieuw

---
+++ Prototype Files/UI (scripts and scene)/Assets/Scripts/CharMenuScript.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class CharMenuScript : MonoBehaviour {
+	
+	public Button left;
+	public Button right;
+	public Button create;
+	public Button select;
+	public GameObject schijf;
+
+	void Start () {
+		left = left.GetComponent<Button> ();
+		right = right.GetComponent<Button> ();
+		create = create.GetComponent<Button> ();
+		select = select.GetComponent<Button> ();
+	}
+	void Update(){
+		GameObject.Find ("CharMenu").GetComponent<AudioSource>().mute = MainMenuScript.soundEffects;
+	}
+	
+	public void next(){
+		schijf.transform.Rotate(new Vector3(0,30,0));
+	}
+
+	public void previous(){
+		schijf.transform.Rotate(new Vector3(0,-30,0));
+	}
+
+	public void newChar(){
+		Application.LoadLevel("NewCharacter");
+	}
+
+	public void choose(){
+		Application.LoadLevel ("MainMenu");
+	}
+
+
+}
