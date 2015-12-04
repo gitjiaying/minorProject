cb36f04242a30f4925011f58b6b330e98edac715
Olivier Hiemstra <Olivier Hiemstra>
2015-12-04 14:00:40 +0100

    NerdHunter 1.0

---
+++ NerdHunter 1.0/Assets/Scripts/Features/PlayerAppearance.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerAppearance: MonoBehaviour {
+	
+	// Use this for initialization
+	private Material [] temp;
+	public GameObject player;
+
+	void Start () {
+	
+	}
+
+	void Update () {
+		rendertextures ();
+	}
+
+	void rendertextures(){
+		temp = player.GetComponent<Renderer>().materials;
+		for (int i=0; i<temp.Length; i++) {
+			temp [i].SetColor ("_Color",(Color)GameManagerScript.playerskinColor);
+		}
+		player.GetComponent<Renderer>().materials = temp;
+	}
+}
---
+++ NerdHunter 1.0/Assets/Scripts/Player/PlayerStamina.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerStamina : MonoBehaviour {
+
+    public int startingStamina = 100;
+    public static float currentStamina;
+    public Slider StaminaSlider;
+    public float RegenerationRate = 5;
+
+    PlayerController playerController;
+
+	void Awake () {
+        
+        currentStamina = startingStamina;
+
+    }
+
+    void Update ()
+    {
+        if (currentStamina != startingStamina)
+        {
+            Regenerate();
+        }
+    }
+
+   
+
+    public void Run (float amount)
+    {
+        currentStamina -= amount*Time.deltaTime;
+        StaminaSlider.value = currentStamina;
+        
+    }
+
+    public void Jump (float amount)
+    {
+        currentStamina -= amount;
+        StaminaSlider.value = currentStamina;
+    }
+
+   void Regenerate()
+    {
+        currentStamina += RegenerationRate * Time.deltaTime;
+        StaminaSlider.value = currentStamina;
+    }
+}
