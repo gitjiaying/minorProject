775b2e46fbae63c6fb1bb15cc5fa2d7decdcc033
Thomas Broens <Thomas Broens>
2016-01-15 16:30:17 +0100

    Sprinter, dynamiek

---
+++ NHEarlyAccess/Assets/Scripts/Nerds/NerdInitiator.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NerdInitiator : MonoBehaviour {
+
+    public NerdsHealth health;
+    public FollowShortestPath follow;
+    public float healthRate;
+    public float speedRate;
+
+
+	
+	void Start ()
+    {
+        int averageHealth = Mathf.RoundToInt(100f + healthRate * GameScene.counter);
+        health.startingHealth = averageHealth;
+
+        int averageSpeed = Mathf.RoundToInt(6f + speedRate * GameScene.counter);
+        follow.speed = Random.Range(averageSpeed - 1, averageSpeed + 1);
+	}
+	
+
+
+}
