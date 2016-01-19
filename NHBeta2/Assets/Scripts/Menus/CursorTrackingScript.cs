d94fbb536dc0445a8d6c2666e62cfec539923d91
AjdinHusic <ajdin@live.nl>
2016-01-19 10:55:24 +0100

    cursorTrackingScript

---
+++ NHBeta2/Assets/Scripts/Menus/CursorTrackingScript.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CursorTrackingScript : MonoBehaviour {
+
+	public CursorMode cursorMode = CursorMode.Auto;
+    public Vector2 hotSpot = Vector2.zero;
+    public Texture2D cursorTexture;
+
+	void Update(){
+		Cursor.SetCursor(cursorTexture, hotSpot, cursorMode);
+	}
+}
